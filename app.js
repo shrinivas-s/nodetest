9b6e430b9c6dce848a1b06b4898b12682ea005a3
Shrinivas S <shrini-3774@shrini-3774.local>
2020-09-05 19:26:17 +0530

    port added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var server=require('http').Server(app);
 app.get("/",function(req,res){
 	 res.send('<h1>Hello world</h1>');
 })
-server.listen(3000, () => {
+const port=process.env.port || 3000;
+server.listen(port, () => {
   console.log('listening on *:3000');
 });
