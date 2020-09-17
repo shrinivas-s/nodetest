2c8fac7ab02002e22b2d1cb01d30e5b8d546b6e9
shrinivas-s <43266832+shrinivas-s@users.noreply.github.com>
2020-09-17 19:47:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var app=require('express')();
 var server=require('http').Server(app);
 app.get("/",function(req,res){
-	 res.send('<h1>Hello world</h1>');
+	 res.sendFile(__dirname+'/index.html');
+})
+app.get("/favicon.ico",function(req,res){
+	 res.sendFile(__dirname+'/favicon.ico');
+})
+app.get("/main.css",function(req,res){
+	 res.sendFile(__dirname+'/main.css');
+})
+app.get("/TamilNadu_Logo.png",function(req,res){
+	 res.sendFile(__dirname+'/TamilNadu_Logo.png');
 })
 const port=process.env.port || 3000;
 server.listen(port, () => {
