a8593b7baeb3bb87e0ef7224b561a5f3e7538c19
shrinivas-s <43266832+shrinivas-s@users.noreply.github.com>
2020-09-17 20:19:53 +0530

    Update app.js
    default port 8080

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/main.css",function(req,res){
 app.get("/TamilNadu_Logo.png",function(req,res){
 	 res.sendFile(__dirname+'/TamilNadu_Logo.png');
 })
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 8080;
 server.listen(port, () => {
-  console.log('listening on *:3000');
+  console.log('Shrinivas listening on *:port');
 });
