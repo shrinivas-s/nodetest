e1a018149d07c36d0b9a707ab9de62fcabfb2171
shrinivas-s <43266832+shrinivas-s@users.noreply.github.com>
2020-09-17 20:19:07 +0530

    Update app.js
    port to PORT

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/main.css",function(req,res){
 app.get("/TamilNadu_Logo.png",function(req,res){
 	 res.sendFile(__dirname+'/TamilNadu_Logo.png');
 })
-const port=process.env.port || 3000;
+const port=process.env.PORT || 3000;
 server.listen(port, () => {
   console.log('listening on *:3000');
 });
