33c194a7665781552caf43a1e3c3eafcf50f498d
shrinivas-s <43266832+shrinivas-s@users.noreply.github.com>
2020-09-17 21:27:15 +0530

    Update app.js
    using static server

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-var app=require('express')();
+var express=require('express');
+var app=express();
 var server=require('http').Server(app);
+
+app.use(express.static('public'))
 app.get("/",function(req,res){
 	 res.sendFile(__dirname+'/index.html');
 })
-app.get("/favicon.ico",function(req,res){
-	 res.sendFile(__dirname+'/favicon.ico');
-})
-app.get("/main.css",function(req,res){
-	 res.sendFile(__dirname+'/main.css');
-})
-app.get("/TamilNadu_Logo.png",function(req,res){
-	 res.sendFile(__dirname+'/TamilNadu_Logo.png');
-})
-app.get("/qr.png",function(req,res){
-	 res.sendFile(__dirname+'/qr.png');
-})
+app.get('/download', function(req, res){
+  const file = `${__dirname}/TN_CHN-DGL_I_1709_516018.pdf`;
+  res.setHeader('Content-type', 'application/pdf');
+  res.download(file);
+});
+
+
 const port=process.env.PORT || 8080;
 server.listen(port, () => {
-  console.log('Shrinivas listening on *:port');
+  console.log('listening on *:3000');
 });
